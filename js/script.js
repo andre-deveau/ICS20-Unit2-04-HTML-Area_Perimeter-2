0f9ded401b0a41e90c28ee3bf49d946d02a777cf
andre-deveau <113055933+andre-deveau@users.noreply.github.com>
2022-10-17 14:39:55 -0400

    init

--- js/script.js
+++ js/script.js
@@ -10,14 +10,15 @@
  */
 function calculate() {
   // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
   const length = parseInt(document.getElementById("length-of-rectangle").value)
   const width = parseInt(document.getElementById("width-of-rectangle").value)
-
   // process
-  const area = length * width
-  const perimeter = 2 * (length + width)
+  const area = base * height / 2
 
   // output
+  document.getElementById('base-of-triangle').innerHTML = 'Area is: ' + area + ' cm²'
   document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
   document.getElementById("perimeter").innerHTML =
     "Perimeter is: " + perimeter + " cm"
